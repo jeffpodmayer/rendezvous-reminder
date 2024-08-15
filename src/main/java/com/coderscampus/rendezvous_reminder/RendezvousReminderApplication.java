8db937502aaeb5954a381ca0cfe769c9d2bee438
Jeffrey Podmayer <jeff.podmayer@gmail.com>
2024-08-15 09:23:49 -0700

    initial commit

---
+++ src/main/java/com/coderscampus/rendezvous_reminder/RendezvousReminderApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.rendezvous_reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendezvousReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RendezvousReminderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/rendezvous_reminder/RendezvousReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.rendezvous_reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendezvousReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
