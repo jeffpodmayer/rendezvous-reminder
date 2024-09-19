c1d3458567ef2ec6974c1fe303dca6b9e1273cc9
jeffpodmayer <jeff.podmayer@gmail.com>
2024-09-19 09:50:07 -0700

    webpages configured

--- src/main/java/com/coderscampus/rendezvous_reminder/web/EmailController.java
+++ src/main/java/com/coderscampus/rendezvous_reminder/web/EmailController.java
@@ -3,12 +3,20 @@ package com.coderscampus.rendezvous_reminder.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EmailController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String getHomepage(){
+        return "home";
+    }
+
+    @PostMapping("/submit-email")
+    public String submitEmail(@RequestParam String email){
+        System.out.println("email submitted");
         return "home";
     }
 }
