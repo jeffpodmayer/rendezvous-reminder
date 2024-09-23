da6b0569979df54b913c377a3f83768629557ae7
jeffpodmayer <jeff.podmayer@gmail.com>
2024-09-23 11:43:06 -0700

    Email saved properly!

--- src/main/java/com/coderscampus/rendezvous_reminder/web/EmailController.java
+++ src/main/java/com/coderscampus/rendezvous_reminder/web/EmailController.java
@@ -6,10 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.annotation.processing.SupportedAnnotationTypes;
 
 @Controller
 public class EmailController {
@@ -17,18 +16,16 @@ public class EmailController {
     @Autowired
     EmailService emailService;
 
-    @GetMapping("/home")
-    public String getHomepage(ModelMap model){
-        model.put("email", "");
+    @GetMapping("/")
+    public String getHomepage(Model model){
+        model.addAttribute("email", new Email());
         return "home";
     }
 
     @PostMapping("/submit-email")
-    public String submitEmail(@RequestParam String email){
-        Email newEmail = new Email();
-        newEmail.setEmailAddress(email); // Set email address in Email entity
-        emailService.save(newEmail); // Save the entity
-        System.out.println("Email submitted: " + email);
+    public String submitEmail(@ModelAttribute("email") Email email){
+        emailService.save(email);
+        System.out.println("Email submitted: " + email.getEmailAddress());
         return "home";
     }
 }
